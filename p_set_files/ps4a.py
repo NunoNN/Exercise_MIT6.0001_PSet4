db2f92757caa6fb85f37d2b765fa2ede9cbb32dc
Nuno Neves <nunomiguel_neves@hotmail.com>
2021-11-15 20:25:02 +0000

    format fix

--- p_set_files/ps4a.py
+++ p_set_files/ps4a.py
@@ -4,7 +4,7 @@
 # Time Spent: x:xx
 
 def get_permutations(sequence):
-    '''
+    """
     Enumerate all permutations of a given string
 
     sequence (string): an arbitrary string to permute. Assume that it is a
@@ -21,7 +21,7 @@ def get_permutations(sequence):
 
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
-    '''
+    """
 
     assert len(sequence) != 0
     if type(sequence) is str:
@@ -45,16 +45,16 @@ def get_permutations(sequence):
             sequence.remove(i)
         return sequence
 
+
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
+    #    #EXAMPLE
+    #    example_input = 'abc'
+    #    print('Input:', example_input)
+    #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    #    print('Actual Output:', get_permutations(example_input))
 
-    pass #delete this line and replace with your code here
+    #    # Put three example test cases here (for your sanity, limit your inputs
+    #    to be three characters or fewer as you will have n! permutations for a
+    #    sequence of length n)
 
+    pass  # delete this line and replace with your code here
