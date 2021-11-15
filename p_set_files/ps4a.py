e53442d8870de4a1c39a16e52e57dfb6ff5b8974
Nuno Neves <nunomiguel_neves@hotmail.com>
2021-11-15 20:20:04 +0000

    create get_permutation function
    recursion reduces the sequence length
    each letter is added in each available position for each permutation
    [d]   -> .d.
    [cd, dc] -> .c.d. __ .d.c.
    [bcd, cbd, cdb, bdc, dbc, dcb] --> ...
    ...

--- p_set_files/ps4a.py
+++ p_set_files/ps4a.py
@@ -16,14 +16,34 @@ def get_permutations(sequence):
     Returns: a list of all permutations of sequence
 
     Example:
-    >>> get_permutations('abc')
-    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+    # >>> get_permutations('abc')
+    # ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
 
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
 
-    pass #delete this line and replace with your code here
+    assert len(sequence) != 0
+    if type(sequence) is str:
+        sequence = list(sequence)
+
+    # base case -> string of length 1 --> returns itself
+    if len(sequence) == 1:
+        # permutations = sequence
+        return sequence
+    else:
+        first_letter = sequence[0]
+        sequence = get_permutations(sequence[1:])
+        # recursion removes letters from the beginning of the string until sequence is length 1
+        # operations after recursion
+        temp = sequence[:]
+        for n in range(len(temp)):
+            for j in range(len(temp[n]) + 1):
+                string = temp[n][0:j] + first_letter + temp[n][j:]
+                sequence.append(string)
+        for i in temp:
+            sequence.remove(i)
+        return sequence
 
 if __name__ == '__main__':
 #    #EXAMPLE
